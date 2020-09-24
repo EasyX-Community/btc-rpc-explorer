a4899b9d22189b38b659cd66b065b84a3a9d1beb
MooCat <leshacat@easyx.cc>
2020-09-24 04:23:09 -0400

    Update coins.js

--- app/coins.js
+++ app/coins.js
@@ -1,9 +1,13 @@
 var btc = require("./coins/btc.js");
 var ltc = require("./coins/ltc.js");
+var ltc = require("./coins/pexa.js");
+var ltc = require("./coins/phl.js");
 
 module.exports = {
 	"BTC": btc,
 	"LTC": ltc,
+	"PEXA": pexa,
+	"PHL": phl,
 
-	"coins":["BTC", "LTC"]
+	"coins":["BTC", "LTC", "PEXA", "PHL"]
 };
